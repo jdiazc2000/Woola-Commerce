dc15daba2d380e4e421dfc7143edaeefd8fc7df4
jdiazc2000 <Julio_Diaz_Cortegana@hotmail.com>
2022-03-22 21:11:13 -0500

    Map title

--- src/pages/sections/map.js
+++ src/pages/sections/map.js
@@ -8,7 +8,7 @@ const Map = () => {
         </div>
         
         <div className="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.5161963603528!2d-77.0852846844901!3d-12.076772745749256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c970336b0725%3A0xae2a6f969b48786e!2sPlaza%20San%20Miguel!5e0!3m2!1ses-419!2spe!4v1645648027375!5m2!1ses-419!2spe"  width="80%" height="450" frameBorder="0" style={{border:  "solid black 2px"}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.5161963603528!2d-77.0852846844901!3d-12.076772745749256!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c970336b0725%3A0xae2a6f969b48786e!2sPlaza%20San%20Miguel!5e0!3m2!1ses-419!2spe!4v1645648027375!5m2!1ses-419!2spe"  width="80%" height="450" frameBorder="0" style={{border:  "solid black 2px"}} title="Ubication" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
         </div>
 
     </div>
