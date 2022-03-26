b35c8a7b890abca7a6c9cff0f79591982c902c11
jdiazc2000 <Julio_Diaz_Cortegana@hotmail.com>
2022-03-25 23:27:56 -0500

    Loading Screen - HOME

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,30 +7,58 @@ import Modals from "./sections/modals"
 import BuyModals from "./sections/modalsbuy"
 import Carousel from "./sections/carousel"
 import { motion } from "framer-motion"
+import {useState,useEffect} from "react"; 
+import GridLoader from "react-spinners/GridLoader"
 
 const Home = () => {
+    const [loading,setLoading] = useState(false);
+
+    useEffect(() =>{
+        setLoading(true);
+        setTimeout(() =>{
+            setLoading(false)
+        },1000)
+    },[])
+
     return (
     <>
+    {
+        loading ?   
+      <div className="LOADERPAGE">  
+        <GridLoader
+            size={30}
+            color="#8F00FF"
+            loading={loading}
+            margin={10}
+        />
+      </div>  
+
+        : 
+<div>          
     <div id="Carousel">
         <Carousel/>
     </div>  
      
     <div id="Ofertas">
-        <div className="title">
-           <motion.div initial={{x:-40}} animate={{ x: 0 }} transition={{ type: "spring", stiffness: 100}}>
-                <h2>Ofertas</h2>
-            </motion.div>
-        </div>
 
-        <div className="Ofertas-section" >
-            <Sales uripath={"catalogo?Oferta_like=SI"}/>
-        </div>  
+            <div className="title">
+                <motion.div initial={{x:-40}} animate={{ x: 0 }} transition={{ type: "spring", stiffness: 100}}>
+                    <h2>Ofertas</h2>
+                </motion.div>
+            </div>
+
+            <div className="Ofertas-section" >
+                <Sales uripath={"catalogo?Oferta_like=SI"}/>
+            </div>  
     
-        <Modals/>
-        <BuyModals/>   
-    </div>
+            <Modals/>
+            <BuyModals/>   
 
-    <Footer/>
+    </div>
+    
+        <Footer/>
+</div>
+    }
     </>
     )
 }
