5d10b05018522d37c4b521d3e8b82049922565ba
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-04-03 10:44:42 -0500

    a

--- src/pages/components/Squares/AboutSquares.js
+++ src/pages/components/Squares/AboutSquares.js
@@ -1,8 +1,16 @@
 
 import { motion } from "framer-motion"
 
+import { defineLordIconElement } from "lord-icon-element";
+import lottie from "lottie-web";
+
+
 const AboutSquares  = ({img,descrip}) => {
+
+    defineLordIconElement(lottie.loadAnimation);
+
     return(
+        <>
 
     <motion.div  initial={{scale:0.8}} animate={{ scale: 1}} transition={{ type: "spring", stiffness: 100 }}>
         <div className="squares" >
@@ -11,7 +19,9 @@ const AboutSquares  = ({img,descrip}) => {
         </div>
     </motion.div>
 
+     <lord-icon trigger="hover" src="/my-icon.json"></lord-icon>
 
+     </>
     )
 } 
 
