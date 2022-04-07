36d2f33ced6a01642f7bfc61fd78919b8da3d592
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-04-07 00:17:22 -0500

    BuyForm-Colors IMGS

--- src/pages/components/ProductBuy/ProductBuy.js
+++ src/pages/components/ProductBuy/ProductBuy.js
@@ -11,23 +11,26 @@ const ProductBuy = ({ id, tipo, marca, imagen, colores, stock, talla }) => {
 
 
   const onSubmit = (data) => {
-    console.table(data); 
+
     setButtontextonbuy(true);
-    const buytimer = setTimeout (() => {
+    const buytimer = setTimeout(() => {
       Swal.fire
-      ( 
-        'Compra realizada',
-        'Pronto nos comunicaremos contigo :)',
-        'success'
-      )
+        (
+          'Compra realizada',
+          'Pronto nos comunicaremos contigo :)',
+          'success'
+        )
       setButtontextonbuy(false);
+
+      console.table(data);
+
       reset();
-    },2000);
+    }, 2000);
     return () => clearTimeout(buytimer);
   }
 
 
-  const BackBuyModal = () =>{
+  const BackBuyModal = () => {
     reset();
   }
 
@@ -55,55 +58,85 @@ const ProductBuy = ({ id, tipo, marca, imagen, colores, stock, talla }) => {
                 <h4>{tipo} {marca}</h4>
               </div>
 
-              <div className="modal-img">
-                <img src={imagen.img1} alt="product-img" />
-              </div>
-
               <Fragment>
+
                 <form id={`Formulario-producto-buy${id}`} onSubmit={handleSubmit(onSubmit)}>
 
                   <select style={{ display: "none" }} name="id_product" title="id_product" {...register('id_product')}>
                     <option defaultValue={id}>{id}</option>
                   </select>
 
-                  <div className="Comboboxes">
+                  <div className="Radio_colors">
                     <div className="Color_div">
-                      <label htmlFor="colores">Color:</label>
-                      <select 
-                      name="colores" 
-                      className="select-color" 
-                      title="color-form" 
-                      {...register('colores')}>
-                        <option defaultValue={colores.color1}>{colores.color1}</option>
-                        <option value={colores.color2}>{colores.color2}</option>
-                        <option value={colores.color3}>{colores.color3}</option>
-                      </select>
-                    </div>
+                      <div className="Color_div_value">
+                          <input
+                            id={`color1-${id}`}
+                            name="colores"
+                            type="radio"
+                            className="input-hidden"
+                            {...register('color')}
+                            defaultValue={colores.color1}
+                          />
+                          <label htmlFor={`color1-${id}`}>
+                          <img src={imagen.img1} alt="imgcolor1"></img>
+                          </label>
+                      </div>
 
+                      <div className="Color_div_value">
+                          <input
+                            id={`color2-${id}`}
+                            name="colores"
+                            type="radio"
+                            className="input-hidden"
+                            {...register('color')}
+                            defaultValue={colores.color2}
+                          />                       
+                          <label htmlFor={`color2-${id}`}>
+                          <img src={imagen.img2} alt="imgcolor2"></img>
+                          </label>
+                      </div>
+
+                      <div className="Color_div_value">
+                          <input
+                            id={`color3-${id}`}
+                            name="colores"
+                            type="radio"
+                            className="input-hidden"
+                            {...register('color')}
+                            defaultValue={colores.color3}
+                          />
+                          <label htmlFor={`color3-${id}`}>
+                          <img src={imagen.img3} alt="imgcolor3"></img>
+                          </label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="ComboBoxes"> 
                     <label htmlFor="talla">Talla:</label>
-                    <select 
-                    name="talla"
-                    title="talla-form"  
-                    {...register('talla')}>
+                    <select
+                      name="talla"
+                      title="talla-form"
+                      {...register('talla')}>
                       <option defaultValue={talla}>{talla}</option>
                     </select>
 
-                  <label htmlFor="amount">Cantidad:</label>
-                    <input 
-                    type="number" 
-                    defaultValue="1" 
-                    min="1" 
-                    max={stock} 
-                    title="stock-buy" 
-                    {...register('stock-buy')} />
-                  </div>
+                    <label htmlFor="amount">Cantidad:</label>
+                    <input
+                      type="number"
+                      defaultValue="1"
+                      min="1"
+                      max={stock}
+                      title="stock-buy"
+                      {...register('stock-buy')} />
+                   </div>
 
                   <div className="name-last">
                     <p>Datos del titular:</p>
-                    <input 
-                    type="text" 
-                    name="nomb_apell" 
-                    placeholder="Nombre y apellido"
+                    <input
+                      type="text"
+                      name="nomb_apell"
+                      placeholder="Nombre y apellido"
                       {...register('nomb_apell', { required: true, pattern: /^[^\s]+( [^\s]+)+$/ })} />
                     {errors.nomb_apell?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
                     {errors.nomb_apell?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese nombres y apellidos validos</span>}
@@ -112,64 +145,64 @@ const ProductBuy = ({ id, tipo, marca, imagen, colores, stock, talla }) => {
                   <div className="creditcard-info">
 
                     <p>Datos de tarjeta:</p>
-                    <input 
-                    type="text"
-                    name="creditcard_number" 
-                    placeholder="0000000000000000"
-                    {...register('creditcard_number', { required: true, pattern: /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$/ })} />
+                    <input
+                      type="text"
+                      name="creditcard_number"
+                      placeholder="0000000000000000"
+                      {...register('creditcard_number', { required: true, pattern: /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})$/ })} />
                     {errors.creditcard_number?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
                     {errors.creditcard_number?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese número de tarjeta válido</span>}
 
 
                     <div className="expi-ccv">
 
-                    <div>
-                        <input 
-                        type="text" 
-                        name="creditcard_expdate"
-                        placeholder="MM/YY" 
-                        {...register('creditcard_expdate', { required: true, pattern: /^(0[1-9]|1[0-2])\/?([0-9]{2})$/ })} />
+                      <div>
+                        <input
+                          type="text"
+                          name="creditcard_expdate"
+                          placeholder="MM/YY"
+                          {...register('creditcard_expdate', { required: true, pattern: /^(0[1-9]|1[0-2])\/?([0-9]{2})$/ })} />
+
+                        <div>
+                          {errors.creditcard_expdate?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
+                          {errors.creditcard_expdate?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese una fecha de expiración válida</span>}
+                        </div>
+                      </div>
+
 
                       <div>
-                        {errors.creditcard_expdate?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
-                        {errors.creditcard_expdate?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese una fecha de expiración válida</span>}
-                      </div>
-                    </div>
+                        <input
+                          type="text"
+                          name="creditcard_ccv"
+                          placeholder="CCV"
+                          {...register('creditcard_ccv', { required: true, pattern: /^[0-9]{3,4}$/ })} />
 
-
-                    <div>
-                      <input
-                       type="text"
-                       name="creditcard_ccv"
-                       placeholder="CCV"
-                       {...register('creditcard_ccv', { required: true, pattern: /^[0-9]{3,4}$/ })} />
-                        
-                      <div>
-                      {errors.creditcard_ccv?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
-                      {errors.creditcard_ccv?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese un CCV válido</span>}
+                        <div>
+                          {errors.creditcard_ccv?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
+                          {errors.creditcard_ccv?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese un CCV válido</span>}
+                        </div>
                       </div>
-                    </div>
-         
+
                     </div>
 
                     <div className="email">
                       <p>Correo electrónico</p>
-                      <input 
-                      type="text" 
-                      name="email_form_buy"
-                      placeholder="Correo"
-                      {...register('email_form_buy', { required: true, pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/ })} />
+                      <input
+                        type="text"
+                        name="email_form_buy"
+                        placeholder="Correo"
+                        {...register('email_form_buy', { required: true, pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/ })} />
                       {errors.email_form_buy?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
                       {errors.email_form_buy?.type === 'pattern' && <span style={{ color: "red" }}>Ingrese un email válido</span>}
                     </div>
 
                     <div className="direction">
                       <p>Dirección</p>
-                      <input 
-                      type="text"
-                      name="direction"
-                      placeholder="Dirección"
-                      {...register('direction', { required: true })} />
+                      <input
+                        type="text"
+                        name="direction"
+                        placeholder="Dirección"
+                        {...register('direction', { required: true })} />
                       {errors.direction?.type === 'required' && <span style={{ color: "red" }}>Por favor rellene este campo</span>}
                     </div>
                   </div>
@@ -177,7 +210,7 @@ const ProductBuy = ({ id, tipo, marca, imagen, colores, stock, talla }) => {
 
                   <div className="modal-footer" style={{ justifyContent: "center" }}>
                     {
-                      buttontextonbuy ? <button className="btn btn-secondary mr-auto">Procesando compra...</button> : <button className="btn btn-secondary mr-auto" >Procesar Compra</button> 
+                      buttontextonbuy ? <button className="btn btn-secondary mr-auto" type="button">Procesando compra...</button> : <button type="submit" className="btn btn-secondary mr-auto" >Procesar Compra</button>
                     }
                     <label className="btn btn-secondary mr-auto volver-btn" data-bs-dismiss="modal" onClick={BackBuyModal} >Volver</label>
                   </div>
--- src/pages/sections/carousel.js
+++ src/pages/sections/carousel.js
@@ -6,16 +6,12 @@ const Carousel = () => {
 
   return (
     <div id="Carousel">
-
-      
+ 
       <div className="Text">
             <motion.div initial={{x:-50}} animate={{ x: 0 }} transition={{ type: "spring", stiffness: 100}}>
                 <h2>Haz tus compras online en Woola Commerce y 
                 encuentra las mejores ofertas en tecnología, muebles, moda, electrohogar, telefonía y deportes.</h2>
             </motion.div>
-      </div>
-      <div className="Images">
-            <img alt="Carousel" src=""/>
       </div>
 
     </div>
