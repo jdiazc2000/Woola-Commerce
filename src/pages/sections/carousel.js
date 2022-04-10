9a1ef7b758290015395854a5d37f966dd8beac37
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-04-10 11:28:45 -0500

    Alerts fixed

--- src/pages/sections/carousel.js
+++ src/pages/sections/carousel.js
@@ -1,10 +1,11 @@
 import "../../styles/styles-home.scss"
 import { motion } from "framer-motion"
-import Img1 from "../imgs/shop-button.png"
+//import Img1 from "../imgs/shop-button.png"
 
 
 const Carousel = () => {
 
+  /*
   const Images = [
     { id: 1, image: require("../imgs/shop-button.png") }
     /*
@@ -12,8 +13,8 @@ const Carousel = () => {
     { id:3, image: require("../imgs/discount-tag.png")},
     { id:4, image: require("../imgs/online-shopping-store.png")},
     { id:5, image: require("../imgs/mega-sale-announcement.png")}
-    */
   ]
+  */
 
   return (
     <div id="Carousel">
