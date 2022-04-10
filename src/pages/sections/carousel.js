c9c15b51b5b2ef78c9ff135065d3872f02455280
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-04-10 12:17:38 -0500

    Text on carousel always on the center of the box

--- src/pages/sections/carousel.js
+++ src/pages/sections/carousel.js
@@ -1,25 +1,12 @@
 import "../../styles/styles-home.scss"
 import { motion } from "framer-motion"
-//import Img1 from "../imgs/shop-button.png"
-
 
 const Carousel = () => {
-
-  /*
-  const Images = [
-    { id: 1, image: require("../imgs/shop-button.png") }
-    /*
-    { id:2, image: require("../imgs/debit-card.png")},
-    { id:3, image: require("../imgs/discount-tag.png")},
-    { id:4, image: require("../imgs/online-shopping-store.png")},
-    { id:5, image: require("../imgs/mega-sale-announcement.png")}
-  ]
-  */
 
   return (
     <div id="Carousel">
 
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ type: "fade", stiffness: 100 }}>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ type: "fade",  duration: 1.5}}>
         <div className="Text_div">
           <div className="Text">
             <h2>Haz tus compras online en Woola Commerce y
@@ -33,9 +20,10 @@ const Carousel = () => {
       </motion.div>
 
       <div className="Carousel_IMG">
-
         <motion.div initial={{ y: 100 }} animate={{ y: 0 }} transition={{ type: "spring", duration: 1.5 }}>
-          <img src="{Img1}" alt="IMG_IMG"></img>
+          
+          <h1>Images</h1>
+
         </motion.div>
       </div>
     </div>
