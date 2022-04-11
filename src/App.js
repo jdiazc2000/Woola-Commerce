aa8cbeedc44de8ba62e58304e4cb7516fd012f54
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-04-11 15:01:51 -0500

    HashRouther TEST 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Page404 from "./pages/Page404"
 import { motion } from "framer-motion"
 import {BrowserRouter  as Router, Route, Routes, NavLink} from "react-router-dom";
 
-
 const openMenu = () => {
   const navmenu = document.querySelector(".nav-menu");
   navmenu.classList.toggle("nav-menu_visible");
@@ -30,15 +29,15 @@ const App = () => (
       <motion.div initial={{x:0}} animate={{ x: 30 }} transition={{ type: "spring", stiffness: 100}}> <h1><span>WOOLA</span> COMMERCE</h1> </motion.div><label onClick={openMenu} className="nav-toogle" title="menu">  <i className="fa fa-solid fa-caret-down"></i>  </label>
           <ul className="nav-menu">
 
-              <li className="nav-menu-item"><NavLink to="/" onClick={CloseMenu} exact="true"  className="nav-link" 
+              <li className="nav-menu-item"><NavLink to="/" onClick={CloseMenu} path="true"  className="nav-link" 
               style={({ isActive }) => (isActive ? {color: '#8F00FF', fontWeight: 'bold' , backgroundColor: '#F0F0F0', borderBottom: '3px solid'} : {color: 'black'})}
               >Inicio</NavLink></li>
 
-              <li className="nav-menu-item"><NavLink to="/aboutus" onClick={CloseMenu} exact path="true"  className="nav-link"
+              <li className="nav-menu-item"><NavLink to="/aboutus" onClick={CloseMenu}  path="true"  className="nav-link"
               style={({ isActive }) => (isActive ? {color: '#8F00FF', fontWeight: 'bold' , backgroundColor: '#F0F0F0', borderBottom: '3px solid'} : {color: 'black'})}
               >Sobre nosotros</NavLink></li>
 
-              <li className="nav-menu-item"><NavLink to="/catalogo" onClick={CloseMenu} exact path="true"  className="nav-link"
+              <li className="nav-menu-item"><NavLink to="/catalogo" onClick={CloseMenu}  path="true"  className="nav-link"
               style={({ isActive }) => (isActive ? {color: '#8F00FF', fontWeight: 'bold' , backgroundColor: '#F0F0F0', borderBottom: '3px solid'} : {color: 'black'})}
               >Catálogo</NavLink></li>
 
