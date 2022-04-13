e648178c6fbc2db7f83aeb143b6d836912ff0073
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-04-13 16:46:25 -0500

    Footer adition

--- src/pages/sections/footer.js
+++ src/pages/sections/footer.js
@@ -1,48 +1,54 @@
 import "../../styles/styles-footer.scss"
-import { NavLink} from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
 
-    const scrollToTop = () =>{
+    const scrollToTop = () => {
         window.scrollTo({
-          top: 0, 
-          behavior: 'smooth'
+            top: 0,
+            behavior: 'smooth'
         });
-      };
+    };
 
     return (
-<>
+        <>
 
-<footer className="footer-distributed">
+            <footer className="footer-distributed">
 
-<div className="footer-right">
+                <div className="footer-right">
 
-    <a href="#/"><i className="fa fa-facebook" title="facebook-link"></i></a>
-    <a href="#/"><i className="fa fa-twitter" title="twitter-link"></i></a>
-    <a href="#/"><i className="fa fa-instagram" title="instagram-link"></i></a>
-    <a href="#/"><i className="fa fa-whatsapp" title="whatsapp-link"></i></a>
+                    <a href="#/"><i className="fa fa-facebook" title="facebook-link"></i></a>
+                    <a href="#/"><i className="fa fa-twitter" title="twitter-link"></i></a>
+                    <a href="#/"><i className="fa fa-instagram" title="instagram-link"></i></a>
+                    <a href="#/"><i className="fa fa-whatsapp" title="whatsapp-link"></i></a>
 
-</div>
+                </div>
 
-<div className="footer-left">
+                <div className="footer-left">
 
-    <p className="footer-links">
+                    <p className="footer-links">
 
-        <NavLink to="/" path="true" onClick={scrollToTop}>Inicio</NavLink> -
+                        <NavLink to="/" path="true" onClick={scrollToTop}>Inicio</NavLink> -
 
-        <NavLink to="/aboutus" path="true" onClick={scrollToTop} >Sobre nosotros</NavLink> -
-        
-        <NavLink to="/catalogo" path="true" onClick={scrollToTop}>Catálogo</NavLink>
+                        <NavLink to="/aboutus" path="true" onClick={scrollToTop} >Sobre nosotros</NavLink> -
 
-    </p>
+                        <NavLink to="/catalogo" path="true" onClick={scrollToTop}>Catálogo</NavLink>
 
-    <p> WOOLA COMMERCE &copy; TODOS LOS DERECHOS RESERVADOS</p>
-</div>
+                    </p>
+                    
+                    <p> WOOLA COMMERCE &copy; TODOS LOS DERECHOS RESERVADOS</p>
 
-</footer>
-</>
-)
+                    <div className="footer-bottom">
+                        <h2>Hecho con <FontAwesomeIcon icon={faHeart} /> por Julio Díaz Cortegana</h2>
+                    </div>
+                </div>
+
+
+            </footer>
+        </>
+    )
 
 }
 
